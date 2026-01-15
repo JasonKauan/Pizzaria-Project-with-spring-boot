a43ca7d1f94d516171d6fa016a85cdd0936e3e57
JasonKauan <jasonkauandiass@gmail.com>
2026-01-15 20:03:37 -0300

    feat : implemeted controller methods

--- src/main/java/com/projeto/pizzaria/demo/controller/OrderController.java
+++ src/main/java/com/projeto/pizzaria/demo/controller/OrderController.java
@@ -1,14 +1,47 @@
 package com.projeto.pizzaria.demo.controller;
 
+import com.projeto.pizzaria.demo.DTO.CreateOrderRequest;
+import com.projeto.pizzaria.demo.DTO.UpdateOrderStatusRequest;
+import com.projeto.pizzaria.demo.entity.Order;
+import com.projeto.pizzaria.demo.service.OrderService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping ("/orders")
 public class OrderController {
-    public ResponseEntity<String> createOrder() {
-        return ResponseEntity.ok("Pedido Criado com Sucesso!");
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody CreateOrderRequest request) {
+        Order order = orderService.createOrder(
+               request.getUserId(),
+                request.getTotal()
+        );
+        return ResponseEntity.ok(order);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable UUID id) {
+        Order order = orderService.getOrderById(id);
+        return ResponseEntity.ok(order);
+
+    }
+
+    @PutMapping("/{orderId}/status")
+    public ResponseEntity<Order> updateOrderStatus(@PathVariable UUID orderId, @RequestBody UpdateOrderStatusRequest request) {
+        Order order = orderService.updateOrderStatus(orderId, request.getStatus());
+        return ResponseEntity.ok(order);
+
+
+
+
     }
 
 }
